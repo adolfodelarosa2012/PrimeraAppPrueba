c832a48ff5634649972c4e92fd4abcd86de84f45
AndreuccioTuo <tavolaroandrea@gmail.com>
2019-07-26 09:42:56 +0200

    Initial Commit

---
+++ PrimeraAppPrueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeraAppPrueba
+//
+//  Created by Dev2 on 26/07/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
